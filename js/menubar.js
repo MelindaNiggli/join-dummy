c9abca98dd635cc4678c394ba8d6920d13036b98
Jörg Habermann <joerg1985@gmx.net>
2024-03-21 17:47:19 +0100

    pages structure

--- js/menubar.js
+++ js/menubar.js
@@ -3,16 +3,29 @@
  * Adding selectedMenu / selectedImg effects to the selected menu element
  * @param {string} menuitem using the id to adress the item
  */
-function menuSelected(menuitem) {
-  document.querySelectorAll(".selectedMenu").forEach((e) => e.classList.remove("selectedMenu"));
-  document.querySelectorAll(".selectedImg").forEach((e) => e.classList.remove("selectedImg"));
+function menuSelected(menuitem) {  
   let menupoint = document.getElementById(menuitem);
   menupoint.classList.add("selectedMenu");
-  if (menuitem == "privacy" || menuitem == "legal") {
+  if (menuitem == "Join-Privacy-Policy" || menuitem == "Join-Legal-Notice") {
     menupoint.style.color = '#cdcdcd';
   } else {
     menupoint.firstChild.classList.add("selectedImg");
   }
+}
+
+async function includeHTML() {
+  let includeElements = document.querySelectorAll("[include-html]");
+  for (let i = 0; i < includeElements.length; i++) {
+    const element = includeElements[i];
+    file = element.getAttribute("include-html"); 
+    let resp = await fetch(file);
+    if (resp.ok) {
+      element.innerHTML = await resp.text();
+    } else {
+      element.innerHTML = "Page not found";
+    }
+  }
+  menuSelected(document.title);
 }
 
 
