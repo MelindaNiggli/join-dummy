04e7512a01945dbfb6226b8582b58ebcca364344
MelindaNiggli <94929141+MelindaNiggli@users.noreply.github.com>
2024-03-24 17:32:38 +0100

    added function login

--- js/login.js
+++ js/login.js
@@ -1,21 +1,44 @@
-// const urlParams = new URLSearchParams(window.location.search);
-// const msg = urlParams.get('msg');
-// const messageBox = document.getElementById('messageBox'); 
 
-// if (msg) {
-//     messageBox.innerHTML = msg; 
-// }
+let users = [];
 
 
-// function login(){
+async function init(){
+    loadUsers();
+}
 
-//     let email = document.getElementById('email');
-//     let password = document.getElementById('password');
-//     let user = users.find( u => u.email == email.value && u.password == password.value);
-//     console.log(user);
-//     if(user){
-//         window.location.href = 'board.html';
-//     }
+async function loadUsers(){
+    try {
+        users = JSON.parse(await getItem('users'));
+    } catch(e){
+        console.error('Loading error:', e);
+    }
+}
+
+const urlParams = new URLSearchParams(window.location.search);
+const msg = urlParams.get('msg');
+const messageBox = document.getElementById('messageBox'); 
+
+if (msg) {
+    messageBox.innerHTML = msg; 
+}
 
 
-// }
+function login(){
+
+    let email = document.getElementById('email');
+    let password = document.getElementById('password');
+    let user = users.find( u => u.email == email.value && u.password == password.value);
+    console.log(user);
+    if(user){
+        window.location.href = 'board.html';
+    }
+
+}
+
+function resetForm() {
+    nameUser.value = '';
+    email.value = '';
+    password.value = '';
+    passwordAgain = '';
+    registerBtn.disabled = false;
+}
