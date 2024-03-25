66eddb0b26396f9d0b7cc02b727e065eddce0106
Jörg Habermann <joerg1985@gmx.net>
2024-03-25 22:36:35 +0100

    drag drop bars

--- js/board.js
+++ js/board.js
@@ -70,9 +70,24 @@ function updateTasks() {
     const task = tasks[i];
     let container = document.getElementById(`task_${task.category}`);
     container.innerHTML += renderTaskHTML(task,i);
-    showAssigned(task.assigned,i);
-  }   
-} 
+    showAssigned(task,task.assigned,i);
+  }  
+  setNoTaskBox(); 
+}
+
+function setNoTaskBox() {
+  for (let i = 0; i < columns.length; i++) {
+    const column = columns[i];
+    const container = document.getElementById(`task_${column}`);
+    if (container.innerHTML === '') {
+      container.innerHTML += `
+      <div class="emptybox flex center">
+        <span>No tasks to do</span>
+      </div>
+      `;
+    }    
+  }
+}
 
 function emptyColumns() {
   let columns = ['todo','progress','feedback','done'];
@@ -84,15 +99,15 @@ function emptyColumns() {
 
 function renderTaskHTML(task,index) {
     return `
-    <div class="taskbox" draggable="true" ondragstart="dragStart(${index})">
+    <div id="id${index}" class="taskbox" draggable="true" ondragstart="dragStart(${index})" ondragover="">
       <div class="${task.label.toLowerCase().split(' ').join('')} flex center">${task.label}</div>
       <div class="flex column gap-ss">
         <h3 class="start">${task.title}</h3>
         <p class="start">${task.description}</p>
       </div>
-      <div class="barbox">
+      <div class="barbox" id="bar${index}">
         <div class="barcontainer">
-          <div class="bar" style="width: ${calcBar(task)}%;"></div>
+          <div class="bar" style="width: ${calcBar(task,index)}%;"></div>
         </div>
         <span>${task.subtasks[0]}/${task.subtasks.length - 1}&nbspSubtasks</span>
       </div>
@@ -112,12 +127,12 @@ function getInitials(user) {
 }
 
 function calcBar(task) {
-  let subtasks = task.subtasks.length - 1;
+  let subtasks = task.subtasks.length - 1;  
   let done = task.subtasks[0];
-  return (subtasks === 0) ? 0 : ((done / subtasks) * 100);
+  return (subtasks === 0) ? 0 : ((done / subtasks) * 100);  
 }
 
-function showAssigned(assigned,index) {
+function showAssigned(task,assigned,index) {
   let container = document.getElementById(`userbox${index}`);
   let length = (assigned.length > 5) ? 5 : assigned.length;
   let left = 0;
@@ -126,8 +141,25 @@ function showAssigned(assigned,index) {
     let id = getInitials(user[0]);
     container.innerHTML += renderAssigned(id, user[1], i, left); 
     left += 24;  
-  }    
+  }
+  if (task.subtasks.length == 1) {
+    document.getElementById(`bar${index}`).style.display = 'none';
+  }
 };
+
+function filterTasks() {
+  let search = document.getElementById('findtask').value;
+  emptyColumns();
+  for (let i = 0; i < tasks.length; i++) {
+    let task = tasks[i];
+    if (task.title.toLowerCase().includes(search.toLowerCase())) {
+      let container = document.getElementById(`task_${task.category}`);
+      container.innerHTML += renderTaskHTML(task,i);
+      showAssigned(task,task.assigned,i);
+    }
+  }
+  setNoTaskBox();
+}
 
 function renderAssigned(id, color, zIndex, left) {
   return `<div class="usertag flex center" style="background-color:${color};z-index:${zIndex};left:${left}px">${id}</div>`;
@@ -140,6 +172,7 @@ function dragTo(category) {
 
 function dragStart(id) {
   draggedElement = id;
+  document.getElementById(`id${id}`).classList.add('fade');
 }
 
 function allowDrop(event) {
