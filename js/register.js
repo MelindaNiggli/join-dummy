df1b52086a8050da0abd09608c32603db786e0b9
MelindaNiggli <94929141+MelindaNiggli@users.noreply.github.com>
2024-03-29 12:16:10 +0100

    logo animation

--- js/register.js
+++ js/register.js
@@ -1,6 +1,23 @@
 
 
 let users = [];
+const colors = [
+    '#0038FF',
+    '#00BEE8',
+    '#1FD7C1',
+    '#6E52FF',
+    '#9327FF', 
+    '#C3FF2B',
+    '#FC71FF',
+    '#FF4646',
+    '#FF5EB3',
+    '#FF745E',
+    '#FF7A00',
+    '#FFA35E',
+    '#FFBB2B',
+    '#FFC701',
+    '#FFE62B'
+];
 
 async function init(){
     loadUsers();
@@ -14,6 +31,14 @@ async function loadUsers(){
     }
 }
 
+function getRandomColor() {
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    return colors[randomIndex];
+}
+
+// Zufällige Farbe auswählen
+let color = getRandomColor();
+
 let registerBtn = document.getElementById('registerBTN')
 async function addUser() {
 
@@ -22,11 +47,12 @@ async function addUser() {
         name: nameUser.value,
         email: email.value,
         password: password.value,
-        passwordAgain: passwordAgain.value
+        passwordAgain: passwordAgain.value,
+        color: color
     });
     await setItem('users', JSON.stringify(users));
     resetForm();
-    window.location.href = 'login.html?msg=Du hast dich erfolgreich registriert';
+    window.location.href = 'login.html?msg=You Signed Up successfully';
 }
 
 function resetForm() {
