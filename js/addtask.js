3150428e92fc002716b61a8b00f5f14330839e69
Jörg Habermann <joerg1985@gmx.net>
2024-04-02 21:27:35 +0200

    add new subtask input functions

--- js/addtask.js
+++ js/addtask.js
@@ -1,24 +1,43 @@
 function selectPrio(prio) {
-  document.querySelectorAll('.priobutton').forEach(b => b.classList.remove('urgentselect','mediumselect','lowselect'));
+  document
+    .querySelectorAll(".priobutton")
+    .forEach((b) =>
+      b.classList.remove("urgentselect", "mediumselect", "lowselect")
+    );
   document.getElementById(prio).classList.add(`${prio}select`);
 }
 
 function toggleDrop(id) {
   let container = document.getElementById(`${id}`);
-  container.firstElementChild.classList.toggle('rotate');
-  container.nextElementSibling.classList.toggle('invis');  
+  container.firstElementChild.classList.toggle("rotate");
+  container.nextElementSibling.classList.toggle("invis");
 }
 
 function checkUser(id) {
-  document.getElementById(`${id}`).lastElementChild.classList.toggle('assigned-checked');
+  document
+    .getElementById(`${id}`)
+    .lastElementChild.classList.toggle("assigned-checked");
 }
 
 function selectCategory(category) {
-  toggleDrop('arrowcategory');
-  document.getElementById('category-input').value = category;
+  toggleDrop("arrowcategory");
+  document.getElementById("category-input").value = category;
 }
 
 function enableSubtasksInput() {
-  let field = document.getElementById('subtasks');
+  let field = document.getElementById("subtasks");
+  let hiddenicons = document.getElementById("subtask-active-icons");
+  let plusicon = hiddenicons.nextElementSibling;
   
+  field.disabled ? field.disabled = false : field.disabled = true;
+  if (field.disabled == false) {field.focus()};
+  hiddenicons.classList.toggle('invis');
+  hiddenicons.classList.toggle('flex');
+  plusicon.classList.toggle('invis');
+  plusicon.classList.toggle('flex');
+}
+
+function clearInput() {
+  document.getElementById('subtasks').value = '';
+  enableSubtasksInput();
 }
