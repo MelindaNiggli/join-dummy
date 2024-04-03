d27830b5eccfab8caf42a82937649fd71e31e764
Jörg Habermann <joerg1985@gmx.net>
2024-04-03 21:23:08 +0200

    subtasks input finish

--- js/addtask.js
+++ js/addtask.js
@@ -41,3 +41,38 @@ function clearInput() {
   document.getElementById('subtasks').value = '';
   enableSubtasksInput();
 }
+
+function editSubtask(id) {
+  let field = document.getElementById(`subtasks-input-c${id}`);
+  let iconboxedit = document.getElementById(`created-subtasks-iconbox${id}`);
+  let iconboxcheck = document.getElementById(`created-subtasks-iconbox${id}`).nextElementSibling;
+  field.disabled = false;
+  field.focus();
+  iconboxedit.classList.toggle('invis');
+  iconboxedit.classList.toggle('flex');
+  iconboxcheck.classList.toggle('invis');
+  iconboxcheck.classList.toggle('flex');  
+}
+
+function checkSubtask(id) {
+  let field = document.getElementById(`subtasks-input-c${id}`);
+  let iconboxedit = document.getElementById(`created-subtasks-iconbox${id}`);
+  let iconboxcheck = document.getElementById(`created-subtasks-iconbox${id}`).nextElementSibling;
+  field.disabled = true;
+  iconboxedit.classList.toggle('invis');
+  iconboxedit.classList.toggle('flex');
+  iconboxcheck.classList.toggle('invis');
+  iconboxcheck.classList.toggle('flex'); 
+}
+
+function deleteSubtask(id) {
+  let field = document.getElementById(`subtasks-input-c${id}`);
+  let iconboxedit = document.getElementById(`created-subtasks-iconbox${id}`);
+  let iconboxcheck = document.getElementById(`created-subtasks-iconbox${id}`).nextElementSibling;
+  field.disabled = true;
+  field.focus();
+  iconboxedit.classList.toggle('invis');
+  iconboxedit.classList.toggle('flex');
+  iconboxcheck.classList.toggle('invis');
+  iconboxcheck.classList.toggle('flex'); 
+}
